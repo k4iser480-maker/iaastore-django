e51d34f7a6fbe6404f8c22cdfd96f4d2a496c3b4
k4iser480 <ckelvin1105@gmail.com>
2026-06-19 00:29:04 -0400

    Upload media and force django to serve media in production

--- ecosite/urls.py
+++ ecosite/urls.py
@@ -36,4 +36,11 @@ urlpatterns = [
     path('transportista/', include('orders.urls_transportista', namespace='transportista')),
 
     path('chat/', views.chat_message, name='chat_message'),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+from django.urls import re_path
+from django.views.static import serve
+
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
